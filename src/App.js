ff5242ee501788030bac35c051a38e457ae4ec94
Becky Schmitthenner <becky.schmitthenner@gmail.com>
2021-03-13 14:04:27 -0500

    additional custom hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import MoodClicker from "./MoodClicker"
+import Counter from "./Counter"
+import Dog from "./Dog"
 
 function App() {
   return (
     <div className="App">
+      <Dog />
+      <Counter />
       <MoodClicker />
     </div>
   );
---
+++ src/hooks/useFields.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const useFields = (initialState) => {
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = e => {
+
+    // could add validations
+    const { value, name } = e.target;
+    setFormData(formData => ({
+      ...formData,
+      [name]: value
+    }))
+  }
+
+  const resetFormData = () => {
+    setFormData(initialState)
+  }
+
+
+//   order is important, remember it
+  return [formData, handleChange, resetFormData];
+}
+
+export default useFields;
